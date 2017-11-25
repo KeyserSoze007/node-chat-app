df10ded7a75c4b846c894f5c4f36628d3d932d22
Satam <satam.fiem@gmail.com>
2017-11-25 19:19:30 +0530

    Adding moustache js template

--- public/js/index.js
+++ public/js/index.js
@@ -10,24 +10,41 @@ var socket= io();
     });
 
     socket.on('newChat', function(newChat){
-        console.log('Got new chat from server', newChat);
-
         var formattedTime = moment(newChat.createdAt).format('h:mm a');
-        var li=jQuery('<li></li>');
+        var template=jQuery('#message-template').html();
+        var html=Mustache.render(template,{
+            text:newChat.text,
+            from:newChat.from,
+            createdAt:formattedTime
+        });
+
+        jQuery('#messages').append(html);
+
+        /*var li=jQuery('<li></li>');
         li.text(`${newChat.from} ${formattedTime}:${newChat.text}`);
-        jQuery('#messages').append(li);
+        jQuery('#messages').append(li);*/
     });
 
 
     socket.on('newLocationMessage', function(message){
-        var li = jQuery('<li></li>');
-        var a = jQuery('<a target="_blank">My Current Location </a>');
         var formattedTime= moment(message.createdAt).format('h:mm a');
+        var template=jQuery('#location-message-template').html();
+        var html=Mustache.render(template,{
+            from:message.from,
+            createdAt:formattedTime,
+            url:message.url
+        });
+
+        jQuery('#messages').append(html);
+
+        /*var li = jQuery('<li></li>');
+        var a = jQuery('<a target="_blank">My Current Location </a>');
+
 
         li.text(`${message.from} ${formattedTime}: `);
         a.attr('href',message.url);
         li.append(a);
-        jQuery('#messages').append(li);
+        jQuery('#messages').append(li);*/
     });
 
     /*socket.emit('createChat',{
